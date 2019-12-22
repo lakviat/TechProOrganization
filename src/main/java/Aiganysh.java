2c6f3ae45189549d0b2aeb7fb09278c90495f290
admin <admin@localhost.com>
2019-12-22 11:15:13 -0600

    New Commit

---
+++ src/main/java/Aiganysh.java
@@ -0,0 +1,20 @@
+public class Aiganysh {
+    public static void main(String[] args) {
+        System.out.println("hello  Nurlan this is Aiganysh writing");
+        System.out.println("hello Aiganysh it's Begimai writing...");
+
+        
+
+        System.out.println("HELO THIS IS NURLAN ANSERTING");
+        System.out.println("Hey! this is Zhazgul");
+
+        System.out.println("thats mine");
+        System.out.println("Hello this is Rabia");
+
+
+        System.out.println("Good evening");
+
+        System.out.println("Hello again! -Rabia");
+
+    }
+}
---
+++ src/main/java/TechProLearning.java
@@ -0,0 +1,31 @@
+public class TechProLearning {
+    public static void main(String[] args) {
+
+        System.out.println("THIS IS MASTER BRANCH");
+        System.out.println("Aiganysh");
+        System.out.println("Aiganysh wright way");
+        System.out.println("Arif");
+        System.out.println("THIS IS NURLAN BRANCH");
+
+        System.out.println("Begimai is here:)");
+        System.out.println("Zhazgul ");
+
+        System.out.println("Rabia");
+
+        System.out.println("Maksym Kryklyvets");
+        System.out.println("Aiganysh");
+
+        // ------------------------------------------- //
+
+        System.out.println("This is second round ARIF");
+        System.out.println("This is second round Begimai");
+        System.out.println("Maksym second round");
+        System.out.println("This is second round Aiganysh");
+        System.out.println("This is second round RABIA");
+        System.out.println("This is second round Zhazgul");
+
+        System.out.println("It was cool!:)");
+        System.out.println("HELLO");
+    }
+}
+
