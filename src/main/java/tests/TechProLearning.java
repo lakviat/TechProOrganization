8275e536c9c843b65d1b9b6b876add38960d3537
admin <admin@localhost.com>
2019-12-22 17:23:02 -0600

    Adding log4j

--- src/main/java/tests/TechProLearning.java
+++ src/main/java/tests/TechProLearning.java
@@ -1,6 +1,10 @@
 package tests;
 
+import java.util.logging.Logger;
+
 public class TechProLearning {
+
+
     public static void main(String[] args) {
 
         System.out.println("THIS IS MASTER BRANCH");
