c26699d2a1af0e65bcbcbb7a34bf5cb035858d35
IsmailovaB <56233988+IsmailovaB@users.noreply.github.com>
2019-12-28 23:16:53 -0600

    Begimais commit

--- src/main/java/utilities/RemoveZeros.java
+++ src/main/java/utilities/RemoveZeros.java
@@ -8,17 +8,25 @@ public class RemoveZeros {
 This method is checking Random Integers and if there are 0 integers,
 this method will remove 0.
  */
-    public static ArrayList<Integer> removeZeros(ArrayList<Integer> arr){
-        ArrayList<Integer> intWithoutZero = new ArrayList<Integer>();
 
-        for (int i=0; i<arr.size(); i++){
-           if(arr.get(i) == 0){
-               arr.remove(i);
-               i--;
-           }
-           intWithoutZero.add(arr.get(i));
-        }
-        return intWithoutZero;
+    public static ArrayList<String > removeZeros(ArrayList<Integer> arr) {
+
+    ArrayList<String> str = new ArrayList<>();
+            for (int i = 0; i < arr.size(); i++) {
+
+                if(arr.get(i)==0){
+                    arr.remove(i);
+                    i--;
+                }
+               String s = arr.get(i)+"";
+                 if (s.contains("0")) {
+                     s = s.replaceAll("0", "");
+                     str.add(s);
+                 }
+                 if(i==20){
+                     System.out.println();
+                 }
+                }
+       return str;
     }
-
 }
