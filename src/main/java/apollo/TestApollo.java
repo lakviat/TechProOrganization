2bb4cd5167703d9742facbbe8c7179c13efdd666
admin <admin@localhost.com>
2019-12-29 18:14:44 -0600

    Updating Master

--- src/main/java/apollo/TestApollo.java
+++ src/main/java/apollo/TestApollo.java
@@ -1,10 +1,10 @@
 package apollo;
 
-import utilities.Duplicates;
-import utilities.RandomNum;
-import utilities.RandomString;
+import utilities.*;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -12,17 +12,22 @@ public class TestApollo {
 
     static private final Logger log = Logger.getLogger(TestApollo.class.getName());
 
+
     public static void main(String[] args) {
-        //System.out.println( Duplicates.duplicatesInteger(RandomNum.randomNum()));
-        //System.out.println(Duplicates.duplicatesString(RandomString.randomString()));
+
+        RandomNum.randomNum();
+        RandomString.randomString();
+
+       RemoveZeros.removeZeros(RandomNum.RandomInteger);
+
+       Duplicates.duplicatesString(RandomString.RandomString); // Ayganysh
+       Duplicates.duplicatesInteger(RandomNum.RandomInteger);  // Maks
+
+       Anagram.anagram(RandomString.RandomString); // Zhazgul
 
 
-        ///RandomNum.randomNum();
-        //RandomString.randomString();
-        //Duplicates.duplicatesInteger(RandomNum.randomNum());
-        //Duplicates.duplicatesString(RandomString.randomString());
 
-        Duplicates.duplicatesString(RandomString.randomString());
+
 
     }
 
